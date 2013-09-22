124fb4c329fd9e7c051806bc84a13009dd767b1c
nick.cosentino <nick.cosentino@magnetforensics.com>
2013-09-22 15:17:09 -0400

    Singleton Examples
    - Porting this example from the other repo.

---
+++ singleton-examples/singleton-examples/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace singleton_examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    internal class ThreadSafeSingleton
+    {
+        private static readonly object _instanceLock = new object();
+        private static ThreadSafeSingleton _instance;
+
+        /// 
+        /// Prevents a default instance of the class from being created.
+        /// 
+        private ThreadSafeSingleton()
+        {
+        }
+
+        /// 
+        /// Gets the singleton instance (thread safe).
+        /// 
+        internal static ThreadSafeSingleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_instanceLock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new ThreadSafeSingleton();
+                        }
+                    }
+                }
+
+                return _instance;
+            }
+        }
+    }
+
+
+    internal class NotThreadSafeSingleton
+    {
+        private static NotThreadSafeSingleton _instance;
+
+        /// 
+        /// Prevents a default instance from being created.
+        /// 
+        private NotThreadSafeSingleton()
+        {
+        }
+
+        /// 
+        /// Gets the singleton instance (not thread safe).
+        /// 
+        internal static NotThreadSafeSingleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new NotThreadSafeSingleton();
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
