aaa7358ab34e71a2139508b3e39a0a0f09b6ee64
Nick Cosentino <n.b.cosentino@gmail.com>
2023-01-29 13:53:26 -0800

    RecordTypesInEarlierVersionsExample
    - Pushing more sample code for record types for an upcoming blog post.

---
+++ RecordTypesInEarlierVersionsExample/RecordTypesInEarlierVersionsExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+
+namespace RecordTypesInEarlierVersionsExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Assembly Runtime: {Assembly.GetExecutingAssembly().ImageRuntimeVersion}");
+
+            var myRecord = new MyData(
+                "Hello world!",
+                123);
+            Console.WriteLine(myRecord);            
+        }
+    }
+
+    internal sealed record MyData(
+        string Value1,
+        int Value2);
+}
