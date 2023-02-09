555149ead921085133bbbb6d3551e77604400732
Nick Cosentino <n.b.cosentino@gmail.com>
2023-02-09 08:17:11 -0800

    Adding async EventHandler Examples
    - Blog article for half of this is here:   https://www.devleader.ca/2023/02/14/async-eventhandlers-a-simple-safety-net-to-the-rescue/

---
+++ AsyncEvents/AsyncEvents.AsyncDelegate/Program.cs
@@ -0,0 +1,83 @@
+﻿#region First Example
+Console.WriteLine("Starting first example...");
+
+var ourOldObject = new OldRaisingObject();
+ourOldObject.Event += async (s, e) =>
+{
+    Console.WriteLine("Starting the event handler...");
+    await TaskThatThrowsAsync();
+    Console.WriteLine("Event handler completed.");
+};
+
+try
+{
+    Console.WriteLine("Raising our event...");
+    ourOldObject.Raise(EventArgs.Empty);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Our exception handler caught: {ex}");
+}
+
+Console.WriteLine("First example complete.");
+#endregion
+
+#region Second Example
+//Console.WriteLine("Starting second example...");
+//var ourFancyObject = new FancyRaisingObject();
+//ourFancyObject.ExplicitAsyncEvent += async (s, e) =>
+//{
+//    Console.WriteLine("Starting the event handler...");
+//    await TaskThatThrowsAsync();
+//    Console.WriteLine("Event handler completed.");
+//};
+
+//try
+//{
+//    Console.WriteLine("Raising our async event...");
+//    await ourFancyObject.RaiseAsync(EventArgs.Empty);
+//}
+//catch (Exception ex)
+//{
+//    Console.WriteLine($"Our exception handler caught: {ex}");
+//}
+
+//Console.WriteLine("Second example complete.");
+#endregion
+
+async Task TaskThatThrowsAsync()
+{
+    Console.WriteLine("Starting task that throws async...");
+    throw new InvalidOperationException("This is our exception");
+};
+
+#region New Way - Setup
+delegate Task AsyncEventHandler<TArgs>(object sender, TArgs e)
+    where TArgs : EventArgs;
+
+class FancyRaisingObject
+{
+    // you could in theory have your own event args here
+    public event AsyncEventHandler<EventArgs> ExplicitAsyncEvent;
+
+
+    public async Task RaiseAsync(EventArgs e)
+    {
+        await ExplicitAsyncEvent?.Invoke(this, e);
+    }
+}
+#endregion
+
+#region Old Way - Setup
+class OldRaisingObject
+{
+    // you could in theory have your own event args here
+    public event EventHandler<EventArgs> Event;
+
+
+    public void Raise(EventArgs e)
+    {
+        Event?.Invoke(this, e);
+    }
+}
+#endregion
---
+++ AsyncEvents/AsyncEvents.Wrapper/Program.cs
@@ -0,0 +1,104 @@
+﻿Console.WriteLine("Starting example...");
+
+var raisingObject = new RaisingObject();
+
+#region Base Case
+//raisingObject.Event += async (s, e) =>
+//{
+//    Console.WriteLine("Starting the event handler...");
+//    await TaskThatThrowsAsync();
+//    Console.WriteLine("Event handler completed.");
+//};
+#endregion
+
+#region Solution 1
+//raisingObject.Event += async (s, e) =>
+//{
+//    Console.WriteLine("Starting the event handler...");
+//    try
+//    {
+//        await TaskThatThrowsAsync();
+//    }
+//    catch (Exception ex)
+//    {
+//        Console.WriteLine($"[Inside Event Handler] Our exception handler caught: {ex}");
+//    }
+//    Console.WriteLine("Event handler completed.");
+//};
+#endregion
+
+#region Solution 2
+raisingObject.Event += EventHandlers.TryAsync<EventArgs>(
+    async (s, e) =>
+    {
+        Console.WriteLine("Starting the event handler...");
+        await TaskThatThrowsAsync();
+        Console.WriteLine("Event handler completed.");
+    },
+    ex => Console.WriteLine($"[TryAsync Error Callback] Our exception handler caught: {ex}"));
+#endregion
+
+try
+{
+    Console.WriteLine("Raising our event...");
+    raisingObject.Raise(EventArgs.Empty);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Our exception handler caught: {ex}");
+}
+
+Console.WriteLine("Example complete.");
+
+async Task TaskThatThrowsAsync()
+{
+    Console.WriteLine("Starting task that throws async...");
+    throw new InvalidOperationException("This is our exception");
+};
+
+class RaisingObject
+{
+    // you could in theory have your own event args here
+    public event EventHandler<EventArgs> Event;
+
+
+    public void Raise(EventArgs e)
+    {
+        Event?.Invoke(this, e);
+    }
+}
+
+#region Wrapper Solution
+static class EventHandlers
+{
+    public static EventHandler<TArgs> TryAsync<TArgs>(
+        Func<object, TArgs, Task> callback,
+        Action<Exception> errorHandler)
+        where TArgs : EventArgs
+            => TryAsync<TArgs>(
+            callback,
+            ex =>
+            {
+                errorHandler.Invoke(ex);
+                return Task.CompletedTask;
+            });
+
+    public static EventHandler<TArgs> TryAsync<TArgs>(
+        Func<object, TArgs, Task> callback,
+        Func<Exception, Task> errorHandler)
+        where TArgs : EventArgs
+    {
+        return new EventHandler<TArgs>(async (object s, TArgs e) =>
+        {
+            try
+            {
+                await callback.Invoke(s, e);
+            }
+            catch (Exception ex)
+            {
+                await errorHandler.Invoke(ex);
+            }
+        });
+    }
+}
+#endregion
