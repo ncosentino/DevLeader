403ffe0e4dc235bdb6c481646bb51a7a511bdcf5
Nick Cosentino <n.b.cosentino@gmail.com>
2023-07-23 14:51:43 -0700

    Record Type Equality

---
+++ RecordTypeEqualityDemo/RecordTypeEqualityDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿MyClass first = new MyClass()
+{
+    X = 1,
+    Y = 2,
+};
+MyClass second = new MyClass()
+{
+    X = 1,
+    Y = 2,
+};
+Console.WriteLine($"MyClass: == {first == second}");
+Console.WriteLine($"MyClass: object.Equals() {object.Equals(first, second)}");
+
+MyRecord firstRecord = new MyRecord(1, 2);
+MyRecord secondRecord = new MyRecord(1, 2);
+Console.WriteLine($"MyRecord: == {firstRecord == secondRecord}");
+Console.WriteLine($"MyRecord: object.Equals() {object.Equals(firstRecord, secondRecord)}");
+
+class MyClass
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+}
+
+record MyRecord(
+    int X,
+    int Y);
