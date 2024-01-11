267e8c551e7b4efcaae47a45fd79dbc4a8b9b91b
Nick Cosentino <n.b.cosentino@gmail.com>
2024-01-11 15:42:28 -0800

    Base64 Encoding

---
+++ Base64Encocoding/Base64Encocoding/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+while (true)
+{
+    Console.Write("Enter a string to encode: ");
+    var input = Console.ReadLine();
+
+    var bytes = Encoding.UTF8.GetBytes(input);
+
+    var base64String = Convert.ToBase64String(bytes);
+
+    Console.WriteLine($"Base64 encoded string: {base64String}");
+    Console.WriteLine();
+
+    Console.Write("Enter a string to decode: ");
+    input = Console.ReadLine();
+    
+    var decoded = Convert.FromBase64String(input);
+    var utf8Decoded = Encoding.UTF8.GetString(decoded);
+
+    Console.WriteLine($"Base64 decoded string: {utf8Decoded}");
+}
