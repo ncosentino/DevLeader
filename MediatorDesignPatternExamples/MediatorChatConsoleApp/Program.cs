59a4aaf980e55a0ba4f95ef1b70e13bf1234a6ca
Nick Cosentino <n.b.cosentino@gmail.com>
2024-01-11 15:37:21 -0800

    Mediator Design Pattern

---
+++ MediatorDesignPatternExamples/MediatorChatConsoleApp/Program.cs
@@ -0,0 +1,72 @@
+﻿ChatMediator mediator = new ChatMediator();
+
+ChatUser user1 = new ChatUser(mediator, "User 1");
+ChatUser user2 = new ChatUser(mediator, "User 2");
+ChatUser user3 = new ChatUser(mediator, "User 3");
+
+mediator.RegisterUser(user1);
+mediator.RegisterUser(user2);
+mediator.RegisterUser(user3);
+
+user1.SendMessage("Hello, everyone!");
+
+public class ChatMediator
+{
+    private readonly List<IUser> _users;
+
+    public ChatMediator()
+    {
+        _users = new List<IUser>();
+    }
+
+    public void RegisterUser(IUser user)
+    {
+        _users.Add(user);
+    }
+
+    public void SendMessage(string senderName, string message)
+    {
+        foreach (var user in _users)
+        {
+            if (!user.Name.Equals(senderName))
+            {
+                user.ReceiveMessage(senderName, message);
+            }
+        }
+    }
+}
+
+public interface IUser
+{
+    public string Name { get; }
+
+    void SendMessage(string message);
+
+    void ReceiveMessage(string senderName, string message);
+}
+
+public class ChatUser : IUser
+{
+    private readonly ChatMediator _mediator;
+
+    public ChatUser(
+        ChatMediator mediator, 
+        string name)
+    {
+        _mediator = mediator;
+        Name = name;
+    }
+
+    public string Name { get; }
+
+    public void SendMessage(string message)
+    {
+        Console.WriteLine($"{Name}: Sending message: {message}");
+        _mediator.SendMessage(senderName: Name, message: message);
+    }
+
+    public void ReceiveMessage(string senderName, string message)
+    {
+        Console.WriteLine($"{Name}: Received message '{message}' {senderName}!");
+    }
+}
---
+++ MediatorDesignPatternExamples/MediatrConsoleApp/Program.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using MediatR;
+
+Console.WriteLine("Creating services...");
+var services = new ServiceCollection();
+services.AddScoped<ChatMessageHandler>();
+services.AddSingleton<UserRegistrar>();
+services.AddMediatR(cfg =>
+{
+    cfg.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
+
+Console.WriteLine("Registering users...");
+var serviceProvider = services.BuildServiceProvider();
+var mediator = serviceProvider.GetRequiredService<IMediator>();
+var userRegistrar = serviceProvider.GetRequiredService<UserRegistrar>();
+
+ChatUser user1 = new ChatUser(mediator, "User 1");
+ChatUser user2 = new ChatUser(mediator, "User 2");
+ChatUser user3 = new ChatUser(mediator, "User 3");
+
+userRegistrar.RegisterUser(user1);
+userRegistrar.RegisterUser(user2);
+userRegistrar.RegisterUser(user3);
+
+Console.WriteLine("Sending first message...");
+await user1.SendMessage("Hello, everyone!");
+
+Console.WriteLine("Sending second message...");
+await user1.SendMessage("Hello again, everyone!");
+
+// Chat message request
+public class ChatMessage : IRequest
+{
+    public string Sender { get; }
+    public string Content { get; }
+
+    public ChatMessage(string sender, string content)
+    {
+        Sender = sender;
+        Content = content;
+    }
+}
+
+// Chat message handler (MediatR creates a new instance
+// every time it handles a message)
+public class ChatMessageHandler : IRequestHandler<ChatMessage>
+{
+    private readonly UserRegistrar _userRegistrar;
+
+    public ChatMessageHandler(UserRegistrar userRegistrar)
+    {
+        Console.WriteLine("Constructing chat message handler");
+        _userRegistrar = userRegistrar;
+    }
+
+    public Task Handle(
+        ChatMessage request,
+        CancellationToken cancellationToken)
+    {
+        Console.WriteLine("Entering message handler...");
+
+        // NOTE: this code isn't thread-safe in that someone in the future
+        // could be trying to register a user while we're enumerating
+        foreach (var user in _userRegistrar.Users)
+        {
+            if (!string.Equals(user.Name, request.Sender))
+            {
+                user.ReceiveMessage(
+                    request.Content,
+                    request.Sender);
+            }
+        }
+
+        Console.WriteLine("Exiting message handler.");
+        return Task.CompletedTask;
+    }
+}
+
+// we use this class to preserve the list of users
+public sealed class UserRegistrar
+{
+    private readonly List<ChatUser> _users;
+
+    public UserRegistrar()
+    {
+        _users = new List<ChatUser>();
+    }
+
+    public IReadOnlyList<ChatUser> Users => _users;
+
+    public void RegisterUser(ChatUser user)
+    {
+        _users.Add(user);
+    }
+}
+
+// Chat user
+public sealed class ChatUser
+{
+    private readonly IMediator _mediator;
+
+    public ChatUser(
+        IMediator mediator, 
+        string name)
+    {
+        _mediator = mediator;
+        this.Name = name;
+    }
+
+    public string Name { get; }
+
+    public async Task SendMessage(string message)
+    {
+        ChatMessage msg = new(Name, message);
+        await _mediator.Send(msg);
+    }
+
+    public void ReceiveMessage(
+        string message, 
+        string sender)
+    {
+        Console.WriteLine(
+            $"{Name} received message from {sender}: " +
+            $"{message}");
+    }
+}
