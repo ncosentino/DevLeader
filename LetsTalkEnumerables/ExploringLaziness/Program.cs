f7681e47afdccd7719292a88504a35fea4dd2281
Nick Cosentino <n.b.cosentino@gmail.com>
2024-01-11 15:41:53 -0800

    Enumerables & Iterators

---
+++ LetsTalkEnumerables/ExploringLaziness/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+
+using Microsoft.Diagnostics.Tracing.StackSources;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+NumberGetter numberGetter = new();
+
+Stopwatch sw = Stopwatch.StartNew();
+numberGetter.GetNumbersLazy().Any();
+Console.WriteLine($"Lazy: {sw.ElapsedMilliseconds}ms");
+sw.Stop();
+
+sw.Restart();
+numberGetter.GetNumbersEager().Any();
+sw.Stop();
+
+Console.WriteLine($"Eager: {sw.ElapsedMilliseconds}ms");
+
+
+class NumberGetter
+{
+    public IEnumerable<int> GetNumbersLazy()
+    {
+        Thread.Sleep(1000);
+        yield return 1;
+        Thread.Sleep(1000);
+        yield return 2;
+        Thread.Sleep(1000);
+        yield return 3;
+    }
+
+    public IEnumerable<int> GetNumbersEager()
+    {
+        Thread.Sleep(3000);
+        return new int[] { 1, 2, 3 };
+    }
+}
---
+++ LetsTalkEnumerables/HoldingResources/Program.cs
@@ -0,0 +1,142 @@
+﻿MyRepository repository = new();
+MyAppLayerService service = new(repository);
+UIControl uiControl = new(service);
+await uiControl.RefreshListContentsAsync();
+
+
+public sealed record Data(
+    int Value);
+
+public sealed class PretendConnectionFactory
+{
+    public PretendConnection OpenNewConnection()
+    {
+        Console.WriteLine("Opening new connection");
+        return new();
+    }
+}
+
+public sealed class PretendConnection : IDisposable
+{
+    public void Dispose()
+    {
+        Console.WriteLine("Connection is disposing");
+    }
+}
+
+public sealed class MyRepository
+{
+    private readonly PretendConnectionFactory _connectionFactory;
+
+    public MyRepository()
+    {
+        _connectionFactory = new();
+    }
+
+    public IEnumerable<Data> GetData()
+    {
+        Console.WriteLine("Starting GetData");
+        try
+        {
+            using PretendConnection connection = _connectionFactory
+                .OpenNewConnection();
+            
+            // BROKEN
+            //return ReadData(connection);
+            
+            foreach (var data in ReadData(connection))
+            {
+                yield return data;
+            }
+        }
+        finally
+        {
+            Console.WriteLine("Ending GetData");
+        }
+    }
+
+    private IEnumerable<Data> ReadData(PretendConnection connection)
+    {
+        var preserveColor = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Starting iterator inside ReadData");
+
+        // pretend we're doing something with the connection
+        // to get the data that we're returning here... this
+        // could be using something like Dapper, EF Core, or
+        // even just a DataReader.
+
+        yield return new(1);
+        yield return new(2);
+        yield return new(3);
+
+        Console.WriteLine("Ending iterator inside ReadData");
+        Console.ForegroundColor = preserveColor;
+    }
+}
+
+
+
+
+
+
+
+public sealed class UIControl
+{
+    private readonly MyAppLayerService _service;
+
+    public UIControl(MyAppLayerService service)
+    {
+        _service = service;
+    }
+
+    public void RefreshListContents()
+    {
+        Console.WriteLine("Starting RefreshListContents on the UI thread!");
+
+        foreach (var data in _service.GetData())
+        {
+            Console.WriteLine($"Data: {data.Value}");
+        }
+
+        Console.WriteLine("Ending RefreshListContents");
+    }
+
+    public async Task RefreshListContentsAsync()
+    {
+        Console.WriteLine("Starting RefreshListContentsAsync on the UI thread!");
+
+        var records = await Task.Run(() =>
+        {
+            Console.WriteLine("Starting Task.Run");
+            var data = _service.GetData();
+            Console.WriteLine("Ending Task.Run");
+            return data;
+        });
+
+        Console.WriteLine("Hopefully we did that all asynchronously, right?!");
+        foreach (var data in records)
+        {
+            Console.WriteLine($"Data: {data.Value}");
+        }
+
+        Console.WriteLine("Ending RefreshListContentsAsync");
+    }
+}
+
+public sealed class MyAppLayerService
+{
+    private readonly MyRepository _repository;
+
+    public MyAppLayerService(MyRepository repository)
+    {
+        _repository = repository;
+    }
+
+    // note that this is kind of nasty "bleeding" entities
+    // from the app layer into the UI layer...
+    public IEnumerable<Data> GetData()
+    {
+        return _repository.GetData();
+    }
+}
