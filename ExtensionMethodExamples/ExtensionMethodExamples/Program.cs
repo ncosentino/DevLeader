1cd7a2b2787bc558f7d4ecc4d17052cd5f94e570
Nick Cosentino <n.b.cosentino@gmail.com>
2024-03-17 13:15:27 -0700

    Adding Simple Extension Method Example

---
+++ ExtensionMethodExamples/ExtensionMethodExamples/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+
+HttpContext ourContext = null; // init...
+var baseUrl = ourContext.GetBaseUrl();
+var url = HttpContextExtensionMethods.GetBaseUrl(ourContext);
+
+public static class HttpContextExtensionMethods
+{
+    public static string GetBaseUrl(
+        this HttpContext context)
+    {
+        var request = context.Request;
+        var host = request.Host;
+        var scheme = request.Scheme;
+        var pathBase = request.PathBase;
+        var url = $"{scheme}://{host}{pathBase}".TrimEnd('/');
+        return url;
+    }
+}
