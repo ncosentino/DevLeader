b80b4e3781ca3921a19a65b8f1c8af54d3d8089a
Nick Cosentino <n.b.cosentino@gmail.com>
2024-03-17 13:14:16 -0700

    Adding in Async Event Handler Examples

---
+++ AsyncEventHandlers/AsyncEventHandlers.ExampleApp/Program.cs
@@ -0,0 +1,55 @@
+﻿EventSource eventSource = new();
+eventSource.Event += async /*void*/ (sender, args) =>
+{    
+    try
+    {
+        await Awaitable();
+        //await NotAwaitable();
+        //NotAwaitable();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(
+            $"We gracefully caught the " +
+            $"exception in the handler: {ex}");
+        throw;
+    }
+};
+
+try
+{
+    eventSource.RaiseEvent();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(
+        $"We gracefully caught the exception " +
+        $"when raising: {ex}");
+}
+
+Console.WriteLine("Press enter to exit.");
+Console.ReadLine();
+
+async void NotAwaitable()
+{
+    await Task.Yield();
+    throw new InvalidOperationException(
+        "Who will catch this for us?");
+}
+
+async Task Awaitable()
+{
+    await Task.Yield();
+    throw new InvalidOperationException(
+        "We can catch this one!");
+}
+
+class EventSource
+{
+    public event EventHandler<EventArgs> Event;
+
+    public void RaiseEvent()
+    {
+        Event?.Invoke(this, EventArgs.Empty);
+    }
+}
