c96b42f8f5ca2120fcec6484a0ef9392ef2477b2
Nick Cosentino <n.b.cosentino@gmail.com>
2024-03-22 10:53:07 -0700

    MongoDB in C# - Inserting Records
    * Code examples for inserting records using MongoDB in C#
    * Examples can be found here:
        * https://www.devleader.ca/2024/03/22/mongodb-in-c-simplified-guide-for-inserting-data/
        * https://youtu.be/0fB9qg-oR04

---
+++ MongoDBExamples/MongoDBExamples.InsertRecords/Program.cs
@@ -0,0 +1,47 @@
+﻿//
+// Example of using MongdoDB in C# for inserting records.
+// More info can be found here:
+// - https://youtu.be/0fB9qg-oR04
+// - https://www.devleader.ca/2024/03/22/mongodb-in-c-simplified-guide-for-inserting-data/
+//
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+/* This is top secret :) */
+#region Connection String
+const string ConnectionString = "mongodb+srv://USERNAME:PASSWORD@HOST";
+#endregion
+
+const string DatabaseName = "DevLeaderTest";
+
+MongoClient client = new(ConnectionString);
+var database = client.GetDatabase(DatabaseName);
+
+Console.WriteLine("Writing records using BsonDocument...");
+var collection = database.GetCollection<BsonDocument>("YouTube");
+collection.InsertOne(new BsonDocument()
+{
+    { "Name", "Nick Cosentino" },
+    { "Channel", "DevLeader" },
+    { "Subscribers", 1_000_000 }
+});
+await collection.InsertOneAsync(new BsonDocument()
+{
+    { "Name", "Nick Cosentino" },
+    { "Channel", "DevLeader" },
+    { "Subscribers", 1_000_000 }
+});
+
+Console.WriteLine("Writing records using record DTO...");
+var collection2 = database.GetCollection<YouTubeInfo>("YouTube");
+collection2.InsertOne(new YouTubeInfo(
+    "Nick Cosentino",
+    "DevLeader",
+    1_000_000));
+
+// async version...
+
+record YouTubeInfo(
+    string Name,
+    string Channel, 
+    int Subscribers);
