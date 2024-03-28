b30edc8f64f0f9c1b3ea97adc6bf4186994facd4
Nick Cosentino <n.b.cosentino@gmail.com>
2024-03-27 22:22:33 -0700

    Update Program.cs
    Adding Article Link in Comments
    
    - Adding link to associated article:
      https://www.devleader.ca/2024/03/28/c-mongodb-insert-benchmarks-what-you-need-to-know/

--- MongoDBExamples/MongoDBExamples.Benchmarks/Program.cs
+++ MongoDBExamples/MongoDBExamples.Benchmarks/Program.cs
@@ -1,4 +1,9 @@
-﻿using BenchmarkDotNet.Running;
+//
+// If you want to follow along with content about these benchmarks, you can
+// check out the information here:
+// https://www.devleader.ca/2024/03/28/c-mongodb-insert-benchmarks-what-you-need-to-know/
+//
+using BenchmarkDotNet.Running;
 
 using System.Reflection;
 
