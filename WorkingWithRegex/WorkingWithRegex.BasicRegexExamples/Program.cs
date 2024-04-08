82e68d0c98fbab7df002ff6bea0b51a54269201c
Nick Cosentino <n.b.cosentino@gmail.com>
2024-04-07 22:15:59 -0700

    Regex Benchmarks
    * Read more here:
     * https://www.devleader.ca/2024/04/08/csharp-regex-performance-how-to-squeeze-out-performance/

---
+++ WorkingWithRegex/WorkingWithRegex.BasicRegexExamples/Program.cs
@@ -0,0 +1,85 @@
+﻿//
+// You can learn more about these examples here:
+// https://www.devleader.ca/2024/04/02/regular-expressions-in-csharp-3-examples-you-need-to-know/
+//
+using System.Text.RegularExpressions;
+
+//StartsWithExample();
+//EndsWithExample();
+IsMatchExample();
+
+void StartsWithExample()
+{
+    Console.WriteLine("StartsWith Example...");
+
+    List<string> inputs = new()
+    {
+        "Hello, World!",
+        "Something something Hello!",
+        "  Hello",
+        "Hello from Dev Leader!",
+    };
+
+    string pattern = "^Hello";
+    Regex regex = new(pattern);
+
+    foreach (var input in inputs)
+    {
+        Match match = regex.Match(input);
+        Console.WriteLine(
+            $"'{input}' {(match.Success ? "did" : "did not")} " +
+            "match the string starting with the pattern.");
+    }
+}
+
+void EndsWithExample()
+{
+    Console.WriteLine("EndsWith Example...");
+
+    List<string> inputs = new()
+    {
+        "Coding", // match
+        "I love programming!", // no match
+        "Coding is fun!", // no match
+        "I love programming", // match
+    };
+
+    string pattern = "ing$";
+    Regex regex = new Regex(pattern);
+
+    foreach (var input in inputs)
+    {
+        Match match = regex.Match(input);
+        Console.WriteLine(
+            $"'{input}' {(match.Success ? "did" : "did not")} " +
+            "match the string ending with the pattern.");
+    }
+}
+
+void IsMatchExample()
+{
+    Console.WriteLine("IsMatch Example...");
+    List<string> inputs = new()
+    {
+        "Nick", // no match
+        "Nick1", // no match
+        "1Nick", // no match
+        "Nick42", // match
+        "42Nick", // match
+        "4Nick2", // match
+        "42", // match
+        "1337", // match
+        "6", // no match
+    };
+
+    string pattern = "[0-9]+[a-zA-Z]*[0-9]+";
+    Regex regex = new Regex(pattern);
+
+    foreach (var input in inputs)
+    {
+        bool isMatch = regex.IsMatch(input);
+        Console.WriteLine(
+            $"'{input}' {(isMatch ? "did" : "did not")} " +
+            "match the pattern.");
+    }
+}
---
+++ WorkingWithRegex/WorkingWithRegex.RegexBenchmarks/Program.cs
@@ -0,0 +1,98 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+BenchmarkRunner.Run(
+    Assembly.GetExecutingAssembly(), 
+    args: args);
+
+[MemoryDiagnoser]
+[MediumRunJob]
+public partial class RegexBenchmarks
+{
+    private const string RegexPattern = @"\b\w*(ing|ed)\b";
+
+    private string? _sourceText;
+    private Regex? _regex;
+    private Regex? _regexCompiled;
+    private Regex? _generatedRegex;
+    private Regex? _generatedRegexCompiled;
+
+    [GeneratedRegex(RegexPattern, RegexOptions.None, "en-US")]
+    private static partial Regex GetGeneratedRegex();
+
+    [GeneratedRegex(RegexPattern, RegexOptions.Compiled, "en-US")]
+    private static partial Regex GetGeneratedRegexCompiled();
+
+    [Params("pg73346.txt")]
+    public string? SourceFileName { get; set; }
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _sourceText = File.ReadAllText(SourceFileName!);
+        
+        _regex = new(RegexPattern);
+        _regexCompiled = new(RegexPattern, RegexOptions.Compiled);
+        _generatedRegex = GetGeneratedRegex();
+        _generatedRegexCompiled = GetGeneratedRegexCompiled();
+    }
+
+    [Benchmark(Baseline = true)]
+    public MatchCollection Static()
+    {
+        return Regex.Matches(_sourceText!, RegexPattern!);
+    }
+
+    [Benchmark]
+    public MatchCollection New()
+    {
+        Regex regex = new(RegexPattern!);
+        return regex.Matches(_sourceText!);
+    }
+
+    [Benchmark]
+    public MatchCollection New_Compiled()
+    {
+        Regex regex = new(RegexPattern!, RegexOptions.Compiled);
+        return regex.Matches(_sourceText!);
+    }
+
+    [Benchmark]
+    public MatchCollection Cached()
+    {
+        return _regex!.Matches(_sourceText!);
+    }
+
+    [Benchmark]
+    public MatchCollection Cached_Compiled()
+    {
+        return _regexCompiled!.Matches(_sourceText!);
+    }
+
+    [Benchmark]
+    public MatchCollection Generated()
+    {
+        return GetGeneratedRegex().Matches(_sourceText!);
+    }
+
+    [Benchmark]
+    public MatchCollection Generated_Cached()
+    {
+        return _generatedRegex!.Matches(_sourceText!);
+    }
+
+    [Benchmark]
+    public MatchCollection Generated_Compiled()
+    {
+        return GetGeneratedRegexCompiled().Matches(_sourceText!);
+    }
+
+    [Benchmark]
+    public MatchCollection Generated_Cached_Compiled()
+    {
+        return _generatedRegexCompiled!.Matches(_sourceText!);
+    }
+}
