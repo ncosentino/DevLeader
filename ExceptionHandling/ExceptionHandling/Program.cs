4b837414fa43be1317a519570561d1cfd4107bfc
Nick Cosentino <nbcosentino@gmail.com>
2024-08-17 15:22:26 -0700

    Handling Unhandled Exceptions

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,26 @@
+﻿AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
+{
+    Console.WriteLine(args.ExceptionObject);
+    Console.WriteLine($"Is Terminating: {args.IsTerminating}");
+};
+
+//throw new InvalidOperationException("Oh no!");
+
+
+TaskScheduler.UnobservedTaskException += (sender, args) =>
+{
+    Console.WriteLine(args.Exception);
+    Console.WriteLine($"Is Observed: {args.Observed}");
+};
+
+Task.Run(() =>
+{
+    Thread.Sleep(3000);
+    throw new InvalidOperationException("Oh no!");
+});
+while (true)
+{
+    await Task.Delay(5000);
+
+    GC.Collect();
+}
