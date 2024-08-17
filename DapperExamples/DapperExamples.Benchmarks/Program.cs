2b3aefe75149d5b49320e032fd3f6317c4626ee8
Nick Cosentino <nbcosentino@gmail.com>
2024-08-17 15:20:09 -0700

    Examples With Dapper

---
+++ DapperExamples/DapperExamples.Benchmarks/Program.cs
@@ -0,0 +1,121 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+using Dapper;
+
+using System.Data;
+using System.Data.SQLite;
+using System.Reflection;
+
+BenchmarkRunner.Run(Assembly.GetExecutingAssembly());
+
+[MemoryDiagnoser]
+[ShortRunJob]
+public class InsertBenchmarks
+{
+    private IDbConnection? _connection;
+
+    [Params(1, 100, 1000, 10000)]
+    public int RecordsCount { get; set; }
+
+    [IterationSetup]
+    public void Setup()
+    {
+        if (File.Exists("test.db"))
+        {
+            File.Delete("test.db");
+        }
+
+        _connection = new SQLiteConnection("Data Source=test.db");
+        _connection.Open();
+        CreateTable(_connection);
+    }
+
+
+    [IterationCleanup]
+    public void Cleanup()
+    {
+        _connection?.Dispose();
+    }
+
+    [Benchmark]
+    public void ClassicInsert()
+    {
+        for (int i = 0; i < RecordsCount; i++)
+        {
+            ClassicInsert(_connection!, new Entry(i, "Alice", 42));
+        }
+    }
+
+    [Benchmark]
+    public async Task DapperInsert_Async()
+    {
+        for (int i = 0; i < RecordsCount; i++)
+        {
+            await DapperInsertAsync(_connection!, new Entry(i, "Alice", 42));
+        }
+    }
+
+    [Benchmark]
+    public void DapperInsert_Sync()
+    {
+        for (int i = 0; i < RecordsCount; i++)
+        {
+            DapperInsert(_connection!, new Entry(i, "Alice", 42));
+        }
+    }
+
+    private static void CreateTable(IDbConnection connection)
+    {
+        using IDbCommand command = connection.CreateCommand();
+        command.CommandText =
+            @"
+            CREATE TABLE IF NOT EXISTS test_table1
+            (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                value INTEGER
+            );
+            CREATE TABLE IF NOT EXISTS test_table2
+            (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                value INTEGER
+            );
+            CREATE TABLE IF NOT EXISTS test_table3
+            (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                value INTEGER
+            );
+            ";
+        command.ExecuteNonQuery();
+    }
+
+    private static void ClassicInsert(IDbConnection connection, Entry entry)
+    {
+        using IDbCommand command = connection.CreateCommand();
+        command.CommandText =
+            "INSERT INTO test_table1 (id, name, value) VALUES (@id, @name, @value)";
+        command.Parameters.Add(new SQLiteParameter("@id", entry.Id));
+        command.Parameters.Add(new SQLiteParameter("@name", entry.Name));
+        command.Parameters.Add(new SQLiteParameter("@value", entry.Value));
+        command.ExecuteNonQuery();
+    }
+
+    private static async Task DapperInsertAsync(IDbConnection connection, Entry entry)
+    {
+        await connection.ExecuteAsync(
+            "INSERT INTO test_table2 (id, name, value) VALUES (@Id, @Name, @Value)",
+            entry);
+    }
+
+    private static void DapperInsert(IDbConnection connection, Entry entry)
+    {
+        connection.Execute(
+            "INSERT INTO test_table3 (id, name, value) VALUES (@Id, @Name, @Value)",
+            entry);
+    }
+}
+
+public record Entry(long Id, string Name, int Value);
---
+++ DapperExamples/DapperExamples.SimpleDapper/Program.cs
@@ -0,0 +1,87 @@
+﻿using Dapper;
+
+using System.Data;
+using System.Data.SQLite;
+
+FileInfo testFile = new("test.db");
+if (testFile.Exists)
+{
+    testFile.Delete();
+}
+
+using SQLiteConnection connection = new SQLiteConnection(
+    $"Data Source='{testFile.FullName}'");
+connection.Open();
+
+await CreateTableAsync(connection);
+
+await ClassicInsertAsync(connection, new Entry(1, "Alice", 42));
+var entry = await ClassicGetByIdAsync(connection, 1);
+Console.WriteLine($"Classic: {entry}");
+
+await DapperInsertAsync(connection, new Entry(2, "Bob", 43));
+entry = await DapperGetByIdAsync(connection, 2);
+Console.WriteLine($"Dapper: {entry}");
+
+static async Task CreateTableAsync(SQLiteConnection connection)
+{
+    using SQLiteCommand command = connection.CreateCommand();
+    command.CommandText =
+        """
+        CREATE TABLE IF NOT EXISTS test_table
+        (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            value INTEGER
+        )
+        """;
+    await command.ExecuteNonQueryAsync();
+}
+
+static async Task ClassicInsertAsync(SQLiteConnection connection, Entry entry)
+{
+    using SQLiteCommand command = connection.CreateCommand();
+    command.CommandText =
+        "INSERT INTO test_table (id, name, value) VALUES (@id, @name, @value)";
+    command.Parameters.AddWithValue("@id", entry.Id);
+    command.Parameters.AddWithValue("@name", entry.Name);
+    command.Parameters.AddWithValue("@value", entry.Value);
+    await command.ExecuteNonQueryAsync();
+}
+
+static async Task DapperInsertAsync(IDbConnection connection, Entry entry)
+{
+    await connection.ExecuteAsync(
+        "INSERT INTO test_table (id, name, value) VALUES (@Id, @Name, @Value)",
+        entry);
+}
+
+static async Task<Entry?> ClassicGetByIdAsync(IDbConnection connection, long id)
+{
+    using var command = connection.CreateCommand();
+    command.CommandText = "SELECT * FROM test_table WHERE id = @id";
+    command.Parameters.AddWithValue("@id", id);
+    using var reader = await command.ExecuteReaderAsync();
+    
+    if (await reader.ReadAsync())
+    {
+        return new Entry(
+            reader.GetInt32(0),
+            reader.GetString(1),
+            reader.GetInt32(2));
+    }
+
+    return null;
+}
+
+static async Task<Entry?> DapperGetByIdAsync(IDbConnection connection, long id)
+{
+    return await connection.QueryFirstOrDefaultAsync<Entry>(
+        "SELECT * FROM test_table WHERE id = @id",
+        new { id });
+}
+
+sealed record Entry(
+    long Id, 
+    string Name, 
+    long Value);
