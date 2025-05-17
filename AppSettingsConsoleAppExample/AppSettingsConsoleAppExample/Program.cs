076aee3ee11801ea131ebce7137b763fe8f1abfe
Nick Cosentino <nbcosentino@gmail.com>
2025-05-17 11:41:41 -0700

    App Settings in Console App

---
+++ AppSettingsConsoleAppExample/AppSettingsConsoleAppExample/Program.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Configuration;
+
+IConfiguration config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+var connectionString = config.GetConnectionString("DefaultConnection");
+var ourSetting = config["OurSetting"];
+
+Console.WriteLine($"Connection string: {connectionString}");
+Console.WriteLine($"Our setting: {ourSetting}");
