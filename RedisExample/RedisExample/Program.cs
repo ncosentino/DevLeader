4b29ae915817956263c0cb12cc4c7538a4e14aaa
Nick Cosentino <nbcosentino@gmail.com>
2025-05-17 11:43:09 -0700

    Trivial Redis Example

---
+++ RedisExample/RedisExample/Program.cs
@@ -0,0 +1,13 @@
+﻿// 1. install docker desktop
+// 2. get redis image (or run directly!)
+// 3. use the StackExchange.Redis NuGet package
+
+using StackExchange.Redis;
+
+using ConnectionMultiplexer multiplexer = ConnectionMultiplexer.Connect("localhost:11337");
+IDatabase db = multiplexer.GetDatabase();
+
+//db.StringSet("mykey", "Dev Leader was here!");
+string? value = db.StringGet("mykey");
+
+Console.WriteLine(value);
