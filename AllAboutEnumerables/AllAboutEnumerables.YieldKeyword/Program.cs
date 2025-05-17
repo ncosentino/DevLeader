4ce7b19d51290b04ae9d03f2705faa28b4d67fd8
Nick Cosentino <nbcosentino@gmail.com>
2025-05-17 11:36:47 -0700

    Yield Keyword
    * Video: https://youtu.be/3UdX6G7a38U?si=InpF-e6KM_eHG2zS

---
+++ AllAboutEnumerables/AllAboutEnumerables.YieldKeyword/Program.cs
@@ -0,0 +1,33 @@
+﻿//int YieldInt()
+//{
+//    yield return 123;
+//}
+
+IEnumerable<int> YieldInts()
+{
+    // open a connection
+    Thread.Sleep(500);
+    // execute the query
+    Thread.Sleep(1000);
+    // read the results and yield them back
+    Thread.Sleep(10);
+
+    return [123, 456];
+}
+
+var yieldIntsResult = YieldInts();
+//Console.WriteLine("Enter to continue...");
+//Console.ReadLine();
+
+var materialized = yieldIntsResult;
+foreach (var number in yieldIntsResult)
+{
+    Console.WriteLine($"Got number {number}.");
+}
+
+foreach (var number in materialized)
+{
+    Console.WriteLine($"Got number {number}.");
+}
+
+return;
