fd6ff7c8626865fdcd41c913f80d0152b7bc5de4
Nick Cosentino <nbcosentino@gmail.com>
2025-05-17 11:35:27 -0700

    Unsafe Accessors:
    * Video 1: https://youtu.be/MCe-q5y59Oc?si=L76tcD_Qo_QVKPlP
    * Video 2: https://youtu.be/jp-PAViwbgg?si=HNCyyoMZtZlRJZYV

---
+++ UnsafeAccessors/UnsafeAccessors.Benchmarks/Program.cs
@@ -0,0 +1,233 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+BenchmarkSwitcher.FromAssembly(Assembly.GetExecutingAssembly()).Run(args);
+
+public class OurType
+{
+    public int InstanceField;
+
+    public int InstanceMethod()
+    {
+        return InstanceField;
+    }
+
+    public int InstanceProperty { get; set; }
+}
+
+[ShortRunJob]
+[MemoryDiagnoser]
+public class UnsafeAccessors_InstantiationBenchmarks
+{
+    [Benchmark(Baseline = true)]
+    public OurType Constructor_Classic()
+    {
+        return new OurType();
+    }
+
+    [Benchmark]
+    public OurType Constructor_ActivatorCreateInstance()
+    {
+        return Activator.CreateInstance<OurType>();
+    }
+
+    [Benchmark]
+    public OurType Constructor_Unsafe()
+    {
+        return UnsafeConstructor();
+    }
+
+    [UnsafeAccessor(UnsafeAccessorKind.Constructor)]
+    extern static OurType UnsafeConstructor();
+}
+
+[ShortRunJob]
+[MemoryDiagnoser]
+public class UnsafeAccessors_GetFieldBenchmarks
+{
+    private FieldInfo? _instanceFieldInfo;
+    private OurType? _instance;
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        _instance = new OurType();
+        _instanceFieldInfo = typeof(OurType).GetField("InstanceField");
+    }
+
+    [Benchmark(Baseline = true)]
+    public int InstanceField_Classic()
+    {
+        return _instance!.InstanceField;
+    }
+
+    [Benchmark]
+    public int InstanceField_Reflection()
+    {
+        return (int)_instanceFieldInfo!.GetValue(_instance)!;
+    }
+
+    [Benchmark]
+    public int InstanceField_Unsafe()
+    {
+        ref int instanceField = ref GetSetInstanceField(_instance!);
+        return instanceField;
+    }
+
+    [UnsafeAccessor(UnsafeAccessorKind.Field, Name = "InstanceField")]
+    extern static ref int GetSetInstanceField(
+        OurType instance);
+}
+
+[ShortRunJob]
+[MemoryDiagnoser]
+public class UnsafeAccessors_SetFieldBenchmarks
+{
+    private OurType? _instance;
+    private FieldInfo? _instanceFieldInfo;
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        _instance = new OurType();
+        _instanceFieldInfo = typeof(OurType).GetField("InstanceField");
+    }
+
+    [Benchmark(Baseline = true)]
+    public void InstanceField_Classic()
+    {
+        _instance!.InstanceField = 123456;
+    }
+
+    [Benchmark]
+    public void InstanceField_Reflection()
+    {
+        _instanceFieldInfo!.SetValue(_instance, 123456);
+    }
+
+    [Benchmark]
+    public void InstanceField_Unsafe()
+    {
+        ref int instanceField = ref GetSetInstanceField(_instance!);
+        instanceField = 123456;
+    }
+
+    [UnsafeAccessor(UnsafeAccessorKind.Field, Name = "InstanceField")]
+    extern static ref int GetSetInstanceField(
+        OurType instance);
+}
+
+[ShortRunJob]
+[MemoryDiagnoser]
+public class UnsafeAccessors_GetPropertyBenchmarks
+{
+    private OurType? _instance;
+    private PropertyInfo? _instancePropertyInfo;
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        _instance = new OurType();
+        _instancePropertyInfo = typeof(OurType).GetProperty("InstanceProperty")!;
+    }
+
+    [Benchmark(Baseline = true)]
+    public int InstanceProperty_Classic()
+    {
+        return _instance!.InstanceProperty;
+    }
+
+    [Benchmark]
+    public int InstanceProperty_Reflection()
+    {
+        return (int)_instancePropertyInfo!.GetValue(_instance)!;
+    }
+
+    [Benchmark]
+    public int InstanceProperty_Unsafe()
+    {
+        return GetInstanceProperty(_instance!);
+    }
+
+    [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "get_InstanceProperty")]
+    extern static int GetInstanceProperty(
+        OurType instance);
+}
+
+[ShortRunJob]
+[MemoryDiagnoser]
+public class UnsafeAccessors_SetPropertyBenchmarks
+{
+    private OurType? _instance;
+    private PropertyInfo? _instancePropertyInfo;
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        _instance = new OurType();
+        _instancePropertyInfo = typeof(OurType).GetProperty("InstanceProperty")!;
+    }
+
+    [Benchmark(Baseline = true)]
+    public void InstanceProperty_Classic()
+    {
+        _instance!.InstanceProperty = 123456;
+    }
+
+    [Benchmark]
+    public void InstanceProperty_Reflection()
+    {
+        _instancePropertyInfo!.SetValue(_instance, 123456);
+    }
+
+    [Benchmark]
+    public void InstanceProperty_Unsafe()
+    {
+        SetInstanceProperty(_instance!, 123456);
+    }
+
+    [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "set_InstanceProperty")]
+    extern static void SetInstanceProperty(
+        OurType instance,
+        int value);
+}
+
+[ShortRunJob]
+[MemoryDiagnoser]
+public class UnsafeAccessors_MethodBenchmarks
+{
+    private OurType? _instance;
+    private MethodInfo? _instanceMethodInfo;
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        _instance = new OurType();
+        _instanceMethodInfo = typeof(OurType).GetMethod("InstanceMethod")!;
+    }
+
+    [Benchmark(Baseline = true)]
+    public int InstanceMethod_Classic()
+    {
+        return _instance!.InstanceMethod();
+    }
+
+    [Benchmark]
+    public int InstanceMethod_Unsafe()
+    {
+        return InvokeMethod(_instance!);
+    }
+
+    [Benchmark]
+    public int InstanceMethod_Reflection()
+    {
+        return (int)_instanceMethodInfo!.Invoke(_instance, null)!;
+    }
+
+    [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "InstanceMethod")]
+    extern static int InvokeMethod(
+        OurType instance);
+}
---
+++ UnsafeAccessors/UnsafeAccessors.ConsoleApp/Program.cs
@@ -0,0 +1,95 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+//var instance = Activator.CreateInstance(
+//    typeof(OurType), 
+//    BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.CreateInstance,
+//    null, 
+//    [42],
+//    null);
+
+//var privateFieldValue = (int)typeof(OurType)
+//    .GetField("_privateField", BindingFlags.NonPublic | BindingFlags.Instance)
+//    .GetValue(instance);
+//Console.WriteLine($"Private field: {privateFieldValue}");
+
+//typeof(OurType)
+//    .GetField("_privateField", BindingFlags.NonPublic | BindingFlags.Instance)
+//    .SetValue(instance, 1337);
+
+//typeof(OurType)
+//    .GetMethod("PrivateMethod", BindingFlags.NonPublic | BindingFlags.Instance)
+//    .Invoke(instance, null);
+
+OurType instance = PrivateCtor(42);
+
+int privatePropertyValue = GetPrivateProperty(instance);
+Console.WriteLine($"Private property: {privatePropertyValue}");
+
+ref int referenceToPrivateField = ref GetSetPrivateField(instance);
+
+ref int referenceToStaticPrivateField = ref GetSetStaticPrivateField(null);
+Console.WriteLine($"Static private field: {referenceToStaticPrivateField}");
+
+PrivateMethod(instance);
+StaticPrivateMethod(null);
+
+referenceToPrivateField = 1337;
+PrivateMethod(instance);
+
+
+#region Unsafe Accessors
+[UnsafeAccessor(UnsafeAccessorKind.Constructor)]
+extern static OurType PrivateCtor(
+    int i);
+
+[UnsafeAccessor(UnsafeAccessorKind.Field, Name = "_privateField")]
+extern static int GetPrivateField(
+    OurType instance);
+
+[UnsafeAccessor(UnsafeAccessorKind.StaticField, Name = "_staticPrivateField")]
+extern static ref int GetSetStaticPrivateField(
+    OurType instance);
+
+[UnsafeAccessor(UnsafeAccessorKind.Method, Name = "get_PrivateProperty")]
+extern static int GetPrivateProperty(
+    OurType instance);
+
+[UnsafeAccessor(UnsafeAccessorKind.Method, Name = "PrivateMethod")]
+extern static void PrivateMethod(
+    OurType instance);
+
+[UnsafeAccessor(UnsafeAccessorKind.StaticMethod, Name = "StaticPrivateMethod")]
+extern static void StaticPrivateMethod(
+    OurType instance);
+
+[UnsafeAccessor(UnsafeAccessorKind.Field, Name = "_privateField")]
+extern static ref int GetSetPrivateField(
+    OurType instance);
+#endregion
+
+public class OurType
+{
+    private static int _staticPrivateField = 123456;
+
+    private int _privateField;
+
+    private OurType(int i)
+    {
+        _privateField = i;
+    }
+
+    private static void StaticPrivateMethod()
+    {
+        Console.WriteLine($"Static private: {_staticPrivateField}");
+    }
+
+    private void PrivateMethod()
+    {
+        Console.WriteLine($"Instance private: {_privateField}");
+    }
+
+    private static int StaticPrivateProperty { get => _staticPrivateField; }
+
+    private int PrivateProperty { get => _privateField; }
+}
