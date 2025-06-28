15ce29307f2919f73c31e57e319fb1476e84f179
Nick Cosentino <nbcosentino@gmail.com>
2025-06-28 10:37:54 -0700

    Microsoft Testing Platform -> MSTest

---
+++ MicrosoftTestingPlatformExamples/MicrosoftTestingPlatformExamples.SystemUnderTestProject/Program.cs
@@ -0,0 +1,31 @@
+﻿SomeDependency dependency = new();
+SystemUnderTest sut = new(dependency);
+var result = sut.FormatWelcomeMessage("Hello");
+
+public sealed class SystemUnderTest
+{
+    private readonly IDependency _dependency;
+
+    public SystemUnderTest(IDependency dependency)
+    {
+        _dependency = dependency;
+    }
+
+    public string FormatWelcomeMessage(string greeting)
+    {
+        return $"{greeting} {_dependency.GetSomething()}!";
+    }
+}
+
+public interface IDependency
+{
+    string GetSomething();
+}
+
+public sealed class SomeDependency : IDependency
+{
+    public string GetSomething()
+    {
+        return "Dev Leader";
+    }
+}
---
+++ MicrosoftTestingPlatformExamples/MicrosoftTestingPlatformExamples.TestsWithMSTest/Tests.cs
@@ -0,0 +1,30 @@
+﻿namespace MicrosoftTestingPlatformExamples.TestsWithMSTest;
+
+[TestClass]
+public sealed class Tests
+{
+    [TestMethod]
+    public void FormatWelcomeMessage_ValidInput_ExpectedOutput()
+    {
+        SomeDependency dependency = new();
+        SystemUnderTest sut = new(dependency);
+
+        var result = sut.FormatWelcomeMessage("Hello");
+        Assert.AreEqual("Hello Dev Leader!", result);
+    }
+
+    [DataTestMethod]
+    [DataRow("Hello", "Hello Dev Leader!")]
+    [DataRow("Welcome", "Welcome Dev Leader!")]
+    [DataRow("Hi", "Hi Dev Leader!")]
+    public void FormatWelcomeMessage_ValidInputs_ExpectedOutputs(
+        string input, 
+        string expected)
+    {
+        SomeDependency dependency = new();
+        SystemUnderTest sut = new(dependency);
+
+        var result = sut.FormatWelcomeMessage(input);
+        Assert.AreEqual(expected, result);
+    }
+}
